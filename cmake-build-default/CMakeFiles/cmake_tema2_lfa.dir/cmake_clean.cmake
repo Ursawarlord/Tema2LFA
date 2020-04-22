9722fc13768ad6c2324b6f439dfe93b1b8cb7239
ursawarlord <francu.madalin01@yahoo.ro>
2020-04-22 19:22:42 +0300

    added build for initial commit

---
+++ cmake-build-default/CMakeFiles/cmake_tema2_lfa.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_tema2_lfa.dir/main.cpp.obj"
+  "cmake_tema2_lfa.exe"
+  "cmake_tema2_lfa.exe.manifest"
+  "cmake_tema2_lfa.lib"
+  "cmake_tema2_lfa.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_tema2_lfa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-default/CMakeFiles/tema2_lfa.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tema2_lfa.dir/main.cpp.obj"
+  "tema2_lfa.exe"
+  "tema2_lfa.exe.manifest"
+  "tema2_lfa.lib"
+  "tema2_lfa.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tema2_lfa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
